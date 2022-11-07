f649caaa3c39ef9c58a9e844f75b1f84aa1227e3
ToLa09 <landgraf-tobias@web.de>
2022-11-07 23:54:26 +0100

    backend: rename ControllerTest to IntegrationTest

--- backend/src/test/java/capstone/backend/BpmnDiagramIntegrationTest.java
+++ backend/src/test/java/capstone/backend/BpmnDiagramIntegrationTest.java
@@ -15,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class BpmnDiagramControllerTest {
+class BpmnDiagramIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
