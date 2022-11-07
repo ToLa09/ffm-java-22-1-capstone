ec3f2860390e0b59d8728d9a68f021a2c2d90b06
ToLa09 <landgraf-tobias@web.de>
2022-11-07 23:37:04 +0100

    backend: add integration tests for GET and POST

---
+++ backend/src/test/java/capstone/backend/BpmnDiagramControllerTest.java
@@ -0,0 +1,65 @@
+package capstone.backend;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class BpmnDiagramControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void getAllDiagrams() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/bpmndiagrams"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+
+    @Test
+    @DirtiesContext
+    void addBpmnDiagram() throws Exception {
+        String responseBody = mockMvc.perform(MockMvcRequestBuilders.post("/api/bpmndiagrams")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                    {
+                        "name": "create bill",
+                        "businessKey": "capstone.bpmn.billing.create-bill",
+                        "xmlFile": "create-bill.xml",
+                        "comment": "first version of billing"
+                    }
+                    """))
+                .andExpect(status().isCreated())
+                .andReturn().getResponse().getContentAsString();
+
+        BpmnDiagram responseObject = objectMapper.readValue(responseBody, BpmnDiagram.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/bpmndiagrams"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                    [
+                        {
+                            "id": "<id>",
+                            "name": "create bill",
+                            "businessKey": "capstone.bpmn.billing.create-bill",
+                            "xmlFile": "create-bill.xml",
+                            "comment": "first version of billing"
+                        }
+                    ]
+                    """.replace("<id>", responseObject.id())));
+    }
+}
