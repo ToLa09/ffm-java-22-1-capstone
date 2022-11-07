ca8b4b0982281e0713a2fa0269778c8750f7d450
ToLa09 <landgraf-tobias@web.de>
2022-11-08 00:13:24 +0100

    frontend: display first List from GET-Request

---
+++ frontend/src/BpmnDiagramModel.ts
@@ -0,0 +1,7 @@
+export type BpmnDiagramModel = {
+    id: string
+    name: string
+    businessKey: string
+    xmlFile: string
+    comment: string
+}
--- frontend/src/DiagramBoard.tsx
+++ frontend/src/DiagramBoard.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {BpmnDiagramModel} from "./BpmnDiagramModel";
+import axios from "axios";
 
 function DiagramBoard() {
+    const [bpmnDiagrams, setBpmnDiagrams] = useState<BpmnDiagramModel[]>([])
+
+    const fetchDiagrams = () => {
+        axios.get("/api/bpmndiagrams")
+            .then(response => response.data)
+            .catch(error => console.error("GET-Error: " + error))
+            .then(setBpmnDiagrams)
+    }
+
+    useEffect(() => {
+        fetchDiagrams()
+    },[])
+
     return (
         <ul>
-            <li>Erstes Diagramm</li>
+            {
+                bpmnDiagrams.map(diagram => {
+                    return <li>{diagram.name}, {diagram.businessKey}, {diagram.xmlFile}, {diagram.comment}</li>
+                })
+            }
         </ul>
     );
 }
