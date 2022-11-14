d18b07af132fef2d0652ad1ee1808c57baa3192c
ToLa09 <landgraf-tobias@web.de>
2022-11-14 22:34:41 +0100

    backend: add service and controller with needed models to fetch data from github repo-api

---
+++ backend/src/main/java/neuefische/capstone/CamundaController.java
@@ -0,0 +1,21 @@
+package capstone.backend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/camundadiagrams")
+@RequiredArgsConstructor
+public class CamundaController {
+
+    private final CamundaService service;
+
+    @GetMapping()
+    public List<BpmnDiagramRepoModel> getAllModels(){
+        return service.fetchBpmnDiagramsFromRepo();
+    }
+}
