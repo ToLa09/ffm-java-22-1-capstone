3059a8ed864cc59bd793ff860f50f521e2e8eec2
ToLa09 <landgraf-tobias@web.de>
2022-11-18 15:48:21 +0100

    frontend: add Input-Fields with conditional rendering to diagram table

--- frontend/src/DiagramLine.tsx
+++ frontend/src/DiagramLine.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BpmnDiagramModel} from "./BpmnDiagramModel";
 import axios from "axios";
 
@@ -9,8 +9,20 @@ export type Props = {
 
 function DiagramLine(props: Props) {
 
-    const handleUpdate = (id: string) => {
-        axios.put("/api/bpmndiagrams/"+id)
+    const[editMode, setEditMode] = useState<boolean>(false)
+    const [name,setName] = useState<string>(props.diagram.name)
+    const [businessKey,setBusinessKey] = useState<string>(props.diagram.businessKey)
+    const [xmlFile,setXmlFile] = useState<string>(props.diagram.xmlFile)
+    const [comment,setComment] = useState<string>(props.diagram.comment)
+
+    const handleUpdate = () => {
+        axios.put("/api/bpmndiagrams/"+props.diagram.id, {
+            "id": props.diagram.id,
+            "name": name,
+            "businessKey": businessKey,
+            "xmlFile": xmlFile,
+            "comment":comment
+        })
             .then(props.fetchDiagrams)
             .catch(error => console.error(error))
     }
@@ -34,12 +46,32 @@ function DiagramLine(props: Props) {
 
     return (
         <tr key={props.diagram.id}>
-            <td>{props.diagram.name}</td>
-            <td>{props.diagram.businessKey}</td>
-            <td>{props.diagram.xmlFile}</td>
-            <td>{props.diagram.comment}</td>
+            {editMode ?
+                <>
+                    <td><input placeholder={"name"} value={name} onChange={(event) => setName(event.target.value)}/></td>
+                    <td><input placeholder={"businessKey"} value={businessKey} onChange={(event) => setBusinessKey(event.target.value)}/></td>
+                    <td><input placeholder={"xmlFile"} value={xmlFile} onChange={(event) => setXmlFile(event.target.value)}/></td>
+                    <td><input placeholder={"comment"} value={comment} onChange={(event) => setComment(event.target.value)}/></td>
+                </>
+                :
+                <>
+                    <td>{props.diagram.name}</td>
+                    <td>{props.diagram.businessKey}</td>
+                    <td>{props.diagram.xmlFile}</td>
+                    <td>{props.diagram.comment}</td>
+                </>
+            }
+
             <td>
-                <button onClick={() => handleUpdate(props.diagram.id)}>Edit</button>
+                {editMode ?
+                    <button onClick={() => {
+                                            setEditMode(false)
+                                            handleUpdate()
+                                            }} >Update
+                    </button>
+                    :
+                    <button onClick={() => setEditMode(true)} >Edit</button>
+                }
                 <button onClick={() => handleDuplicate(props.diagram)}>Duplicate</button>
                 <button onClick={() => handleDelete(props.diagram.id)}>Delete</button>
             </td>
