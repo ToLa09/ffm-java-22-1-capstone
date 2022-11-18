520d04ed8f50e02e4cf4ad09173219830eac27f0
ToLa09 <landgraf-tobias@web.de>
2022-11-18 12:12:40 +0100

    backend: add PUT and DELETE-Endpoints with service-methods

--- backend/src/main/java/capstone/backend/BpmnDiagramController.java
+++ backend/src/main/java/capstone/backend/BpmnDiagramController.java
@@ -3,6 +3,7 @@ package capstone.backend;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -20,7 +21,19 @@ public class BpmnDiagramController {
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public BpmnDiagram addBpmnDiagram(@RequestBody BpmnDiagram bpmnDiagram) {
-        return service.addBpmnDiagram(bpmnDiagram);
+    public BpmnDiagram addBpmnDiagram(@RequestBody BpmnDiagram newBpmnDiagram) {
+        return service.addBpmnDiagram(newBpmnDiagram);
+    }
+
+    @PutMapping("/{id}")
+    public BpmnDiagram updateBpmnDiagram(@RequestBody BpmnDiagram updatedBpmnDiagram, @PathVariable String id) {
+        if(updatedBpmnDiagram.id().equals(id)){
+            return service.updateBpmnDiagram(updatedBpmnDiagram);
+        } else throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Request ID Mismatch");
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBpmnDiagram(@PathVariable String id) {
+        service.deleteBpmnDiagram(id);
     }
 }
