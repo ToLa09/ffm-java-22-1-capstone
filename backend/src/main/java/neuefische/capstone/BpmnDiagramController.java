d44aac357f67623d2dacf9c938130141a51b59ca
ToLa09 <landgraf-tobias@web.de>
2022-11-21 21:47:25 +0100

    backend: validate attributes from BpmnDiagram record

--- backend/src/main/java/neuefische/capstone/BpmnDiagramController.java
+++ backend/src/main/java/neuefische/capstone/BpmnDiagramController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,12 +22,12 @@ public class BpmnDiagramController {
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public BpmnDiagram addBpmnDiagram(@RequestBody BpmnDiagram newBpmnDiagram) {
+    public BpmnDiagram addBpmnDiagram(@RequestBody @Valid BpmnDiagram newBpmnDiagram) {
         return service.addBpmnDiagram(newBpmnDiagram);
     }
 
     @PutMapping("/{id}")
-    public BpmnDiagram updateBpmnDiagram(@RequestBody BpmnDiagram updatedBpmnDiagram, @PathVariable String id) {
+    public BpmnDiagram updateBpmnDiagram(@RequestBody @Valid BpmnDiagram updatedBpmnDiagram, @PathVariable String id) {
         if(updatedBpmnDiagram.id().equals(id)){
             return service.updateBpmnDiagram(updatedBpmnDiagram);
         } else throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Request ID Mismatch");
