e204d9beaf46ff91f9b61b26c3fa8080d62b2daf
ToLa09 <landgraf-tobias@web.de>
2022-11-21 21:47:25 +0100

    backend: validate attributes from BpmnDiagram record

--- backend/src/main/java/neuefische/capstone/BpmnDiagram.java
+++ backend/src/main/java/neuefische/capstone/BpmnDiagram.java
@@ -2,11 +2,16 @@ package neuefische.capstone;
 
 import lombok.With;
 
+import javax.validation.constraints.NotEmpty;
+
 @With
 public record BpmnDiagram(
         String id,
+        @NotEmpty
         String name,
+        @NotEmpty
         String businessKey,
+        @NotEmpty
         String xmlFile,
         String comment
 //        Instant timestamp,
