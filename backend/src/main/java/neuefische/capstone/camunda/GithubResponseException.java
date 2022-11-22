30c6144a63dbb0a9b416dbb9fbc72dae68410e21
ToLa09 <landgraf-tobias@web.de>
2022-11-22 17:30:00 +0100

    backend: rename custom exception

--- backend/src/main/java/neuefische/capstone/camunda/GithubResponseException.java
+++
@@ -1,7 +0,0 @@
-package neuefische.capstone.camunda;
-
-public class GithubResponseException extends RuntimeException {
-    public GithubResponseException(String message) {
-        super(message);
-    }
-}
