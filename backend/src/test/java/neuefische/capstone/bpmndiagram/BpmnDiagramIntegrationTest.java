06ee95ebd1767558d5d826985612e57606a0013d
ToLa09 <landgraf-tobias@web.de>
2022-11-23 20:35:19 +0100

    backend: move tests into packages

--- backend/src/test/java/neuefische/capstone/bpmndiagram/BpmnDiagramIntegrationTest.java
+++ backend/src/test/java/neuefische/capstone/bpmndiagram/BpmnDiagramIntegrationTest.java
@@ -1,7 +1,6 @@
-package neuefische.capstone;
+package neuefische.capstone.bpmndiagram;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import neuefische.capstone.bpmndiagram.BpmnDiagram;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
