ff268e8f63be8654f0981b9c42c677c1b5a36b3d
ToLa09 <landgraf-tobias@web.de>
2022-11-24 15:30:07 +0100

    frontend: show Custom Diagram property in Overview

--- frontend/src/DiagramTable.tsx
+++ frontend/src/DiagramTable.tsx
@@ -48,7 +48,7 @@ function DiagramTable(props: DiagramTableProps) {
                             <TableCell sx={{fontWeight: "bold"}} >Name</TableCell>
                             <TableCell sx={{fontWeight: "bold"}} align="left">Filename</TableCell>
                             <TableCell sx={{fontWeight: "bold"}} align="center">latest version</TableCell>
-                            <TableCell sx={{fontWeight: "bold"}} align="center">latest comment</TableCell>
+                            <TableCell sx={{fontWeight: "bold"}} align="center">Custom Diagram (editable)</TableCell>
                             <TableCell sx={{fontWeight: "bold", width: 120}} align="center">Actions</TableCell>
                         </TableRow>
                     </TableHead>
