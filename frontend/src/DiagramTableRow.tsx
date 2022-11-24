59a972ae4fde8450fc875f2d4dd40bc5494089a6
ToLa09 <landgraf-tobias@web.de>
2022-11-24 15:07:06 +0100

    frontend: delete unused useStates

--- frontend/src/DiagramTableRow.tsx
+++ frontend/src/DiagramTableRow.tsx
@@ -1,4 +1,4 @@
-import React, {Dispatch, SetStateAction, useState} from 'react';
+import React, {Dispatch, SetStateAction} from 'react';
 import {BpmnDiagramModel} from "./model/BpmnDiagramModel";
 import './css/DiagramLine.css';
 import TableCell from "@mui/material/TableCell";
@@ -14,9 +14,6 @@ export type Props = {
 
 export default function DiagramTableRow(props: Props) {
 
-    const[editMode, setEditMode] = useState<boolean>(false)
-    const[unfold, setUnfold] = useState<boolean>(false)
-
     const handleDetails = () => {
         props.setValue("Details")
         props.setDetailedDiagram(props.diagram)
@@ -31,46 +28,6 @@ export default function DiagramTableRow(props: Props) {
                 <TableCell align="left">{props.diagram.commentText}</TableCell>
                 <TableCell align="center"><Button onClick={handleDetails} color="secondary">Details</Button></TableCell>
             </TableRow>
-            {/*<TableRow key={props.diagram.id}>*/}
-                {/*{ editMode ?*/}
-                {/*    <>*/}
-                {/*        <TableCell component="td" scope="row"><input placeholder={"name"} value={name} onChange={(event) => setName(event.target.value)}/></TableCell>*/}
-                {/*        <TableCell align="right"><input placeholder={"businessKey"} value={businessKey} onChange={(event) => setBusinessKey(event.target.value)}/></TableCell>*/}
-                {/*        <TableCell align="right"><input placeholder={"xmlFile"} value={filename} onChange={(event) => setFilename(event.target.value)}/></TableCell>*/}
-                {/*        <TableCell align="right"><input placeholder={"comment"} value={comment} onChange={(event) => setComment(event.target.value)}/></TableCell>*/}
-                {/*    </>*/}
-                {/*    :*/}
-                {/*    <>*/}
-                {/*        <TableCell>*/}
-                {/*            <IconButton*/}
-                {/*                aria-label="expand row"*/}
-                {/*                size="small"*/}
-                {/*                onClick={() => setUnfold(!unfold)}*/}
-                {/*            >*/}
-                {/*                {unfold ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}*/}
-                {/*            </IconButton>*/}
-                {/*        </TableCell>*/}
-                {/*        <TableCell component="td" scope="row">{props.diagram.name}</TableCell>*/}
-                {/*        <TableCell align="right">{props.diagram.filename}</TableCell>*/}
-                {/*        <TableCell align="right">{props.diagram.version}</TableCell>*/}
-                {/*        <TableCell align="right">{props.diagram.commentText}</TableCell>*/}
-                {/*    </>*/}
-                {/*}*/}
-                {/*<TableCell component="th" scope="row">*/}
-                {/*    {editMode ?*/}
-                {/*        <IconButton onClick={() => {*/}
-                {/*            setEditMode(false)*/}
-                {/*            handleUpdate()*/}
-                {/*        }} >*/}
-                {/*            <CheckIcon color="secondary"/>*/}
-                {/*        </IconButton>*/}
-                {/*        :*/}
-                {/*        <IconButton onClick={() => setEditMode(true)}><EditIcon color="primary" /></IconButton>*/}
-                {/*    }*/}
-                {/*    <IconButton onClick={() => handleDuplicate(props.diagram)}><ContentCopyIcon color="secondary" /></IconButton>*/}
-                {/*    <IconButton onClick={() => handleDelete(props.diagram.id)}><DeleteForeverIcon color="error" /></IconButton>*/}
-                {/*</TableCell>*/}
-            {/*</TableRow>*/}
         </>
     );
 }
