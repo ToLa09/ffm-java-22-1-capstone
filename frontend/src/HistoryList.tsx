40402f209ab1d05f9603d2a1cf5897420a23f8f2
ToLa09 <landgraf-tobias@web.de>
2022-11-25 17:34:03 +0100

    frontend: move Delete Buttons to HistoryList

--- frontend/src/HistoryList.tsx
+++ frontend/src/HistoryList.tsx
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, {Dispatch, SetStateAction} from 'react';
 import {BpmnDiagramModel} from "./model/BpmnDiagramModel";
 import {Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
+import HistoryListRow from "./model/HistoryListRow";
 
 type HistoryListProps = {
     history: BpmnDiagramModel[]
+    latestDiagram: BpmnDiagramModel
+    customDiagram: boolean
+    setSnackbarOpen: Dispatch<SetStateAction<boolean>>
+    setSnackbarMessage: Dispatch<SetStateAction<string>>
+    fetchHistory: () => void
+    fetchDiagrams: () => void
+    setTab: Dispatch<SetStateAction<string>>
 }
 
 function HistoryList(props: HistoryListProps) {
+
+
     return (
-        <Table>
+        <Table size="small">
             <TableHead>
                 <TableRow>
                     <TableCell sx={{fontWeight: "bold"}}>Name</TableCell>
                     <TableCell sx={{fontWeight: "bold"}} align="center">Filename</TableCell>
                     <TableCell sx={{fontWeight: "bold"}} align="center">version</TableCell>
+                    {props.customDiagram &&
+                        <TableCell sx={{fontWeight: "bold"}} align="right">Delete</TableCell>
+                    }
                 </TableRow>
             </TableHead>
             <TableBody>
-                {
-                    props.history.map(diagram => {
-                        return (
-                            <TableRow>
-                                <TableCell>{diagram.name}</TableCell>
-                                <TableCell align="center">{diagram.businessKey}</TableCell>
-                                <TableCell align="center">{diagram.version}</TableCell>
-                            </TableRow>
-                        )
-                    })
-                }
+                {props.history.map(diagram => <HistoryListRow
+                    key={diagram.id}
+                    diagram={diagram}
+                    fetchHistory={props.fetchHistory}
+                    fetchDiagrams={props.fetchDiagrams}
+                    latestDiagram={props.latestDiagram}
+                    setTab={props.setTab}
+                />)}
             </TableBody>
         </Table>
     );
---
+++ frontend/src/model/HistoryListRow.tsx
@@ -0,0 +1,41 @@
+import React, {Dispatch, SetStateAction} from 'react';
+import {IconButton, TableCell, TableRow} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {BpmnDiagramModel} from "./BpmnDiagramModel";
+import axios from "axios";
+
+type HistoryListRowProps = {
+    diagram: BpmnDiagramModel
+    latestDiagram: BpmnDiagramModel
+    fetchHistory: () => void
+    fetchDiagrams: () => void
+    setTab: Dispatch<SetStateAction<string>>
+}
+
+function HistoryListRow(props: HistoryListRowProps) {
+
+    const handleDelete = () => {
+        axios.delete("/api/bpmndiagrams/" + props.diagram.id)
+            .then(() => {
+                if (props.diagram.id === props.latestDiagram.id) {
+                    props.fetchDiagrams()
+                    props.setTab("Overview")
+                } else props.fetchHistory()
+            })
+    }
+
+    return (
+        <TableRow key={props.diagram.businessKey}>
+            <TableCell>{props.diagram.name}</TableCell>
+            <TableCell align="center">{props.diagram.businessKey}</TableCell>
+            <TableCell align="center">{props.diagram.version}</TableCell>
+            {props.diagram.customDiagram &&
+                <TableCell align="right">
+                    <IconButton color="error" onClick={handleDelete}><DeleteIcon/></IconButton>
+                </TableCell>
+            }
+        </TableRow>
+    );
+}
+
+export default HistoryListRow;
