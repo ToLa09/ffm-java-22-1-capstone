04214623c0c2cbc1d2ed5bd6fe14112f02c5ea80
ToLa09 <landgraf-tobias@web.de>
2022-11-25 13:12:48 +0100

    backend: add GET-Endpoint and methods for list with latest versions only

--- backend/src/main/java/neuefische/capstone/bpmndiagram/BpmnDiagramService.java
+++ backend/src/main/java/neuefische/capstone/bpmndiagram/BpmnDiagramService.java
@@ -3,6 +3,8 @@ package neuefische.capstone.bpmndiagram;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -15,6 +17,19 @@ public class BpmnDiagramService {
 
     public List<BpmnDiagram> getAllDiagrams() {
         return repository.findAll();
+    }
+
+    public List<BpmnDiagram> getLatestDiagrams() {
+        List<BpmnDiagram> allDiagrams = repository.findAll();
+        List<BpmnDiagram> latestDiagrams = new ArrayList<>();
+
+        for (BpmnDiagram diagram : allDiagrams) {
+            BpmnDiagram latestVersion = repository.findAllByBusinessKey(diagram.businessKey()).stream()
+                    .max(Comparator.comparing(BpmnDiagram::version))
+                    .orElseThrow(() -> new NoSuchElementException("No max Version"));
+            latestDiagrams.add(latestVersion);
+        }
+        return latestDiagrams.stream().distinct().toList();
     }
 
     public BpmnDiagram addBpmnDiagram(BpmnDiagram newBpmnDiagram) {
