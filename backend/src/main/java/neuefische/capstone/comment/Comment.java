9406778c22739d39c4ecef1695e89555f190e863
ToLa09 <landgraf-tobias@web.de>
2022-11-30 11:44:38 +0100

    feat: add validation to comment record

--- backend/src/main/java/neuefische/capstone/comment/Comment.java
+++ backend/src/main/java/neuefische/capstone/comment/Comment.java
@@ -2,14 +2,18 @@ package neuefische.capstone.comment;
 
 import lombok.With;
 
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
 @With
 public record Comment(
         String id,
+        @NotEmpty
         String content,
+        @NotEmpty
         String author,
         LocalDateTime time,
+        @NotEmpty
         String diagramId
 ) {
 }
