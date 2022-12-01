2c685904f9154733dfd503a58e18228ca4aed955
ToLa09 <landgraf-tobias@web.de>
2022-12-01 22:05:54 +0100

    refactor: delete diagramId attribute from comment record

--- frontend/src/components/CommentListSimple.tsx
+++ frontend/src/components/CommentListSimple.tsx
@@ -24,11 +24,11 @@ function CommentListSimple(props: CommentListProps) {
     return (
         <>
             {commentList
-                // .sort((comment1, comment2) => {
-                //     if (comment1.time > comment2.time) {
-                //         return -1
-                //     } else return 0
-                // })
+                .sort((comment1, comment2) => {
+                    if (comment1.time > comment2.time) {
+                        return -1
+                    } else return 0
+                })
                 .map(comment => {
                     return <Typography key={comment.id}
                                        variant="body2">{moment(comment.time).format("DD.MM.YYYY HH:MM")} from {comment.author}: {comment.content}</Typography>
