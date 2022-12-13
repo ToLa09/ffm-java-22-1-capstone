bde0989e27ccccae1ce68f80dfce01e9210ef318
ToLa09 <landgraf-tobias@web.de>
2022-12-13 16:15:10 +0100

    refactor: reduce DB-actions, put webclient into generic method, omit method getCalledDiagrams

--- backend/src/main/java/neuefische/capstone/bpmndiagram/BpmnDiagramRepository.java
+++ backend/src/main/java/neuefische/capstone/bpmndiagram/BpmnDiagramRepository.java
@@ -3,9 +3,12 @@ package neuefische.capstone.bpmndiagram;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Set;
 
 public interface BpmnDiagramRepository extends MongoRepository<BpmnDiagram, String> {
     List<BpmnDiagram> findAllByBusinessKey(String businessKey);
 
     List<BpmnDiagram> findAllByCustomDiagram(boolean customDiagram);
+
+    void deleteByIdNotIn(Set<String> processIds);
 }
